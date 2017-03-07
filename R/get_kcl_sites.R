5ba7dabcddc217d704b6e11cb15623219a050af0
Justin Lingard <JustinLingard@users.noreply.github.com>
2017-03-07 19:16:06 +0000

    Delete get_kcl_sites.R

--- R/get_kcl_sites.R
+++
@@ -1,7 +0,0 @@
-#' 
-#' @ JJNL : 13/12/2016
-#' 
-#' This function gets the KCL/LAQN site meta data for all sites,
-#' not necessarily all are in London
-
-kcl_sites <- openair::importMeta(source = "kcl", all = TRUE)
